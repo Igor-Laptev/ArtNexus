6fd6ff688add461ff8db5e2f0969e12d7b24eeac
CanaveralEnvy <oldelevennn@gmail.com>
2024-02-09 13:22:43 +0300

    navbar and main page

--- client/src/features/navbar/NavBar.tsx
+++ client/src/features/navbar/NavBar.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import './navbar.scss';
 
-const NavBar = (props) => {
-  return <div></div>;
-};
+import { Outlet } from 'react-router-dom';
+
+function NavBar(): JSX.Element {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="links">
+          <a className="" href="#">
+            Home <span className="sr-only">(current)</span>
+          </a>
+          <a className="" href="#">
+            Features
+          </a>
+          <a className="" href="#">
+            Pricing
+          </a>
+          <a className="" href="#">
+            Disabled
+          </a>
+        </div>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
 
 export default NavBar;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App/App';
-import './index.css';
 // import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
   // <Provider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   // </Provider>,
   // </React.StrictMode>,
 );
